ae59d90eff5bb17954db5abe4533c966505370a6
David Yu <davidyu1994@gmail.com>
2026-06-25 13:15:50 -0400

    Implement connect page

--- src/routes/connect/+page.svelte
+++ src/routes/connect/+page.svelte
@@ -1,1 +1,9 @@
-<h1>Connect</h1>
+<script lang="ts">
+	import FormShell from '$lib/components/FormShell.svelte'
+	import ConnectForm from '$lib/components/forms/ConnectForm.svelte'
+	import { strings, themes } from '$lib/data/content'
+</script>
+
+<FormShell header={strings.connectHeader} title={strings.connectTitle} description={strings.connectDescription} color={themes.connect}>
+	<ConnectForm />
+</FormShell>
