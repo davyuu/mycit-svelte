f90a05cd7f6fc019ad7cdcd57216e3e4ef6bf317
David Yu <davidyu1994@gmail.com>
2026-06-25 13:16:24 -0400

    Implement request page

--- src/routes/prayer/+page.svelte
+++ src/routes/prayer/+page.svelte
@@ -1,1 +1,9 @@
-<h1>Request</h1>
+<script lang="ts">
+	import FormShell from '$lib/components/FormShell.svelte'
+	import PrayerForm from '$lib/components/forms/PrayerForm.svelte'
+	import { strings, themes } from '$lib/data/content'
+</script>
+
+<FormShell header={strings.prayerHeader} title={strings.prayerTitle} description={strings.prayerDescription} color={themes.prayer}>
+	<PrayerForm />
+</FormShell>
