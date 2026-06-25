1e8d1b2aa407c40fcd1dceb95de05f13e91bc398
David Yu <davidyu1994@gmail.com>
2026-06-25 13:17:53 -0400

    Implement next steps page

--- src/routes/next/+page.svelte
+++ src/routes/next/+page.svelte
@@ -1,1 +1,9 @@
-<h1>Next</h1>
+<script lang="ts">
+	import FormShell from '$lib/components/FormShell.svelte'
+	import SignupForm from '$lib/components/forms/SignupForm.svelte'
+	import { nextOptions, strings, themes } from '$lib/data/content'
+</script>
+
+<FormShell header={strings.nextHeader} title={strings.nextTitle} description={strings.nextDescription} color={themes.next}>
+	<SignupForm type="next" color={themes.next} options={nextOptions} submitLabel="Take My Next Step" />
+</FormShell>
