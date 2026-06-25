fa8d627f645466a4730d242da2d77693ad1ce0e6
David Yu <davidyu1994@gmail.com>
2026-06-25 13:18:42 -0400

    Add confirmation page

---
+++ src/routes/confirm/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import StaticPage from '$lib/components/StaticPage.svelte'
+	import { strings, themes } from '$lib/data/content'
+</script>
+
+<StaticPage header={strings.confirmTitle} title={strings.confirmTitle} description={strings.confirmDescription} color={themes.connect}>
+	<a class="action-link" href="/">Back Home</a>
+</StaticPage>
