077bfcb1c32e64d28b43cec73290a146da10de0a
David Yu <davidyu1994@gmail.com>
2026-06-25 13:19:30 -0400

    Implement youth page

--- src/routes/youth/+page.svelte
+++ src/routes/youth/+page.svelte
@@ -1,1 +1,8 @@
-<h1>Youth</h1>
+<script lang="ts">
+	import StaticPage from '$lib/components/StaticPage.svelte'
+	import { strings, themes } from '$lib/data/content'
+</script>
+
+<StaticPage header={strings.youthHeader} title={strings.youthHeader} description="Stay connected with CITYouth." color={themes.youth}>
+	<a class="action-link" href="https://instagram.com/cityouth_?utm_medium=copy_link" target="_blank" rel="noreferrer">Open Instagram</a>
+</StaticPage>
