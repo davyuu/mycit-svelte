6e02315e3b2847dd88f1af9f154d0ecf046bc9c0
David Yu <davidyu1994@gmail.com>
2026-06-25 13:19:03 -0400

    Add giving page

---
+++ src/routes/giving/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import StaticPage from '$lib/components/StaticPage.svelte'
+	import { strings, themes } from '$lib/data/content'
+</script>
+
+<StaticPage header={strings.givingHeader} title={strings.givingTitle} description={strings.givingDescription} color={themes.giving}>
+	<p>You can give online through Church Center.</p>
+	<a class="action-link" href="https://churchintoronto.churchcenter.com/giving" target="_blank" rel="noreferrer">Give Online</a>
+</StaticPage>
